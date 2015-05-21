9b70a4eb52eb2394e77292bce91a5bd31baaa7f0
Thaddeus R. Winker <trwinker@gmail.com>
2015-05-21 06:38:05 -0400

    Initial commit
    version 1; works but more features pending.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace RollDiced
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Roll = new System.Windows.Forms.Button();
+            this.txt_Roll = new System.Windows.Forms.TextBox();
+            this.results = new System.Windows.Forms.TextBox();
+            this.isWild = new System.Windows.Forms.CheckBox();
+            this.isCritical = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btn_Roll
+            // 
+            this.btn_Roll.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Roll.Location = new System.Drawing.Point(149, 44);
+            this.btn_Roll.Name = "btn_Roll";
+            this.btn_Roll.Size = new System.Drawing.Size(123, 44);
+            this.btn_Roll.TabIndex = 1;
+            this.btn_Roll.Text = "Roll Dice";
+            this.btn_Roll.UseVisualStyleBackColor = true;
+            this.btn_Roll.Click += new System.EventHandler(this.btn_Roll_Click);
+            // 
+            // txt_Roll
+            // 
+            this.txt_Roll.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Roll.Location = new System.Drawing.Point(149, 12);
+            this.txt_Roll.Name = "txt_Roll";
+            this.txt_Roll.Size = new System.Drawing.Size(123, 26);
+            this.txt_Roll.TabIndex = 0;
+            // 
+            // results
+            // 
+            this.results.Cursor = System.Windows.Forms.Cursors.Default;
+            this.results.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.results.Location = new System.Drawing.Point(12, 94);
+            this.results.Multiline = true;
+            this.results.Name = "results";
+            this.results.ReadOnly = true;
+            this.results.Size = new System.Drawing.Size(260, 155);
+            this.results.TabIndex = 2;
+            // 
+            // isWild
+            // 
+            this.isWild.AutoSize = true;
+            this.isWild.Location = new System.Drawing.Point(12, 12);
+            this.isWild.Name = "isWild";
+            this.isWild.Size = new System.Drawing.Size(66, 17);
+            this.isWild.TabIndex = 3;
+            this.isWild.Text = "Wild Die";
+            this.isWild.UseVisualStyleBackColor = true;
+            // 
+            // isCritical
+            // 
+            this.isCritical.AutoSize = true;
+            this.isCritical.Location = new System.Drawing.Point(12, 35);
+            this.isCritical.Name = "isCritical";
+            this.isCritical.Size = new System.Drawing.Size(76, 17);
+            this.isCritical.TabIndex = 4;
+            this.isCritical.Text = "Critical Fail";
+            this.isCritical.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.isCritical);
+            this.Controls.Add(this.isWild);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.txt_Roll);
+            this.Controls.Add(this.btn_Roll);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Dice Roller";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Roll;
+        private System.Windows.Forms.TextBox txt_Roll;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.CheckBox isWild;
+        private System.Windows.Forms.CheckBox isCritical;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace RollDiced
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String rollDice(int[] d)
+        {
+            String roll = "";
+            for (int i = 0; i < d.Count(); i++)
+            {
+                roll += d[i].ToString();
+                if (i != d.Count() - 1)
+                {
+                    roll += " + ";
+                }
+            }
+            roll += " = " + d.Sum();
+            return roll;
+        }
+        public int countDice(String s)
+        {
+            int toD = s.IndexOf('d');
+            if (toD < 1)
+            {
+                return 0;
+            }
+            return int.Parse(s.Substring(0,toD));
+        }
+        public int findDiceType(String s)
+        {
+            int toD = s.IndexOf('d');
+            int len = s.Length;
+            if (toD < 1)
+            {
+                return 0;
+            }
+            return int.Parse(s.Substring(toD + 1, len - (toD + 1)));
+        }
+        public bool validateRoll(String r)
+        {
+            if(r == string.Empty) return false;
+            if (r.Count(s => s == 'd') != 1) return false;
+            if (r.IndexOf('d') < 1) return false;
+            return true;
+        }
+        public int rollWild(int i)
+        {
+            Random rnd = new Random();
+            int roll = i;
+            int wildRoll = roll;
+            while (roll == i)
+            {
+                roll = rnd.Next(1, i);
+                wildRoll += roll;
+            }
+            return wildRoll;
+        }
+        private void btn_Roll_Click(object sender, EventArgs e)
+        {
+            if (!validateRoll(this.txt_Roll.Text))
+            {
+                MessageBox.Show("Please enter a roll designation such as 3d6.");
+                return;
+            }
+            //String path = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\diceroll.wav";
+            //using (SoundPlayer p = new SoundPlayer(path))
+            //{
+            //    p.PlaySync();
+            //};
+            int dDiceCount;
+            int dDiceType;
+            String damage;
+            Random rndRoll = new Random();
+            damage = this.txt_Roll.Text;
+            //split damage into two ints.
+            dDiceCount = countDice(damage);
+            dDiceType = findDiceType(damage);
+            int[] rolls = new int[dDiceCount];
+            for (int i = 0; i < dDiceCount; i ++)
+            {
+                rolls[i] = rndRoll.Next(1,dDiceType+1);
+                if (i == dDiceCount - 1)
+                {
+                    if (isWild.Checked && (rolls[i] == dDiceType))
+                    {
+                        rolls[i] = rollWild(rolls[i]);
+                    }
+                    if (isCritical.Checked && (rolls[i] == 1))
+                    {
+                        rolls[i] = 0;
+                        int maxRoll = rolls.Max();
+                        bool f = false;
+                        int j = 0;
+                        while (!f)
+                        {
+                            if (rolls[j] == maxRoll)
+                            {
+                                rolls[j] = 0;
+                                f = true;
+                            }
+                            j++;
+                        }
+                        
+                    }
+                }
+            }
+            this.results.Text = rollDice(rolls);
+        }
+    }
+}
