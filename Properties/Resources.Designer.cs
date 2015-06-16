fb0988f46928e51e8e2536ed76f65c6527519ec7
Thaddeus R. Winker <trwinker@gmail.com>
2015-06-16 19:46:53 -0400

    minor updates
    Changed the namespace and assembly.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RollDiced.Properties {
+namespace RollDice.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RollDiced.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RollDiced.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RollDice.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
